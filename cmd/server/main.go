d4bda7da2cc23fa854a218cb97ce3f3c405bfb44
Arkkan El-Khatib <arkkanito@gmail.com>
2024-05-17 13:13:01 -0400

    added chi dependency and basic index endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "GET index")
+}
+
+type Config struct {
+	Port int
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	cfg := &Config{
+		Port: 8080,
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+
+	router.Get("/", Index)
+
+	fmt.Printf("Serving on port %d\n", cfg.Port)
+	http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), router)
 }
